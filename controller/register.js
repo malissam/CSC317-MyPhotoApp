59fa9e9f2b6aa6e38437e6ab98cdcd33f9ba1f50
Malissa Martinez de Murga <37825977+malissam@users.noreply.github.com>
2019-12-17 13:58:34 -0800

    NOT really sure what ive added. but its broken anyway -___-
    LOL

--- controller/register.js
+++ controller/register.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var path = require('path');
-var fileUpload = require('express-fileupload')
-
-var DB = require('../myDatabase')
+var fileUpload = require('express-fileupload');
+var cookieParser = require('cookie-parser');
+var DB = require('../myDatabase');
 /* GET users listing. */
 
 // create application/json parser
@@ -13,22 +13,42 @@ var jsonParser = bodyParser.json();
 
 router.post('/register', function(req, res) {
     console.log(req.body);
-    res.redirect("../profile.html");
+//   res.redirect("../profile.html");
 
-    let queryStatment = "INSERT INTO `photoposting`.`user` (`user_name`, `user_email`, `password`) VALUES ('"+req.body.username+"', '"+req.body.username+"', '"+req.body.password+"');"
-    //console.log("THIS IS THE STATEMENT:" + queryStatment);
+    let queryStatment = "INSERT INTO `photoposting`.`user` (`user_name`, `user_email`, `password`) VALUES ('"+req.body.username+"', '"+req.body.email+"', '"+req.body.password+"');"
+    console.log("THIS IS THE STATEMENT:" + queryStatment);
     DB.query(queryStatment, function(error, result, fields){
         console.log("THIS IS THE STATEMENT:" + queryStatment);
+    //    res.send(error);
         //console.log(result);
     });
 });   
 
 //added for sessions
 router.use(session({
+    key: 'user_sessionID',
 	secret: 'secret',
 	resave: true,
-	saveUninitialized: true
+    saveUninitialized: true,
+    cookie:{expires:6000000}
 }));
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sessionID) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }    
+};
+// route for user logout
+router.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sessionID) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json());
 
@@ -48,10 +68,17 @@ router.post('/login',function(req,res)
     console.log(results.length);
     if (results.length > 0){
         req.session.loggedin = true;
-res.cookie
-        res.redirect('../profile.html');
+        req.session.username = login;
+        
+        //res.cookie('login_status', req.session.loggedin) // options is optional
+        console.log("image posted")
+        
+        
+        //end add for cookie
+
+    //    res.redirect('../profile.html');
     }else{
-        res.send('Nope.');
+       // res.send('Nope.');
         res.redirect('../login.html')
     }
 });
@@ -66,19 +93,55 @@ router.get('/home', function(req, response) {
 	response.end();
 });
 
-router.post('/post',function(req,res)
+// default options
+router.use(fileUpload());
+
+router.post('/post',function(req,res,error)
 {
-    var url = req.body.image;
-    var description = req.body.description;
-    var title = req.body.title;
+    console.log(req.files.postedImage);
+    console.log(req.body);
+    console.log(error)
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+      }
+    if(req.session.loggedin){
+     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+      let sampleFile = req.files.postedImage;
+      let photopath = '/public/pictures/'+sampleFile;
+    
+     // Use the mv() method to place the file somewhere on your server
+      sampleFile.mv('public/pictures/'+sampleFile.name, function(err) {
+        if (err)
+        {return res.status(500).send(err);}
+    
+        let queryInsert = "INSERT INTO `photoposting`.`post` (`post_title`, `description`, `pic_url`,`user_id`) VALUES ('"+req.body.title+"', '"+req.body.description+"', '"+sampleFile.name+"','"+1+"');"
+        DB.query(queryInsert, function(error, result, fields){
+            // res.send(error);
+            if(error) {
+                res.send(error);
+            } else {
+                res.send('File uploaded!');
 
-    DB.query("INSERT INTO `photoposting`.`post` (`post_title`, `post_url`) VALUES ('"+title+"', '"+url+"'", function(error,results,fields){
-
-    console.log(results);
-
+            }
+         });  
 });
+    }
 });
 
+
+router.get('/showPicture',function(req,res)
+{
+    var login = req.body.login_username;
+    var password = req.body.login_password;
+    
+    DB.query("SELECT pic_url FROM post;", function(error,results,fields) {
+        console.log(results.map(result => result.pic_url));
+        res.send({
+            'images': results.map(result => "pictures/" + result.pic_url)
+        })
+    });
+    
+});
 
 
 module.exports = router;
--- masterScript.sql
+++ masterScript.sql
@@ -1,29 +1,27 @@
-CREATE DATABASE `photoPosting`;
+CREATE DATABASE IF NOT EXISTS `photoPosting`;
 USE `photoPosting`;
 
-CREATE Table `user` (
+CREATE TABLE IF NOT EXISTS `user`(
     `user_id` INT NOT NULL AUTO_INCREMENT,
+    `password` varchar(50) NOT NULL,
     `user_name` VARCHAR(64) NOT NULL,
     `user_email` VARCHAR(64) NOT NULL,
-    PRIMARY KEY(`user_id`)
+    UNIQUE KEY `username_UNIQUE` (`user_id`)
 );
 
-CREATE TABLE `post`(
+CREATE TABLE IF NOT EXISTS `post`(
     `post_id` INT NOT NULL AUTO_INCREMENT,
     `post_title` VARCHAR(64),
     `user_id` INT,
-    `pic_url` NVARCHAR(2083),
+    `pic_url` VARCHAR(2083),
     `timestamp` TIMESTAMP DEFAULT NULL,
-    PRIMARY KEY (`post_id`)
+	UNIQUE KEY `postID_UNIQUE` (`post_id`)
 );
 
-CREATE TABLE `comment`(
+CREATE TABLE IF NOT EXISTS `comment`(
     `com_id` INT NOT NULL AUTO_INCREMENT,
     `com_string` INT NOT NULL,
     `user_id` INT,
     `posted_on` INT NOT NULL,
-    PRIMARY KEY(`com_id`)
+	UNIQUE KEY `commentID_UNIQUE` (`com_id`)
 );
-
-
-
